b6973671ada50ee9869c708a167fd7c3456ad28d
shawndrost <sdrost@gmail.com>
2012-11-21 00:36:20 -0800

    Add hashFunction

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,7 +1,23 @@
 // Note: don't use an object to store the inserted elements.
+var hashFunction;
 var makeHashTable = function(){
   var hashTable = {
+    hashFunction: hashFunction
     //fixme
   };
   return hashTable;
+}
+
+
+// DON'T WORRY ABOUT CODE BELOW HERE
+
+hashFunction = function(str, maxResult){
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash<<5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash % maxResult;
 }
