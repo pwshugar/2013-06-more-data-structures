9ccf2963524b0985493bcc3631f6c08e49e3d0a9
Shawn Drost <sdrost@gmail.com>
2012-12-03 16:07:44 -0800

    fix hashFunction

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,5 +1,4 @@
 // Note: don't use an object to store the inserted elements.
-var hashFunction;
 var makeHashTable = function(){
   var hashTable = {
     hashFunction: hashFunction
@@ -11,11 +10,11 @@ var makeHashTable = function(){
 
 // DON'T WORRY ABOUT CODE BELOW HERE
 
-hashFunction = function(str, maxResult){
+var hashFunction = function(str, maxResult){
   var hash = 0;
-  if (this.length == 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    char = this.charCodeAt(i);
+  if (str.length == 0) return hash;
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i);
     hash = ((hash<<5) - hash) + char;
     hash = hash & hash; // Convert to 32bit integer
   }
