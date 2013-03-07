d41d332c9fe661aff8c1af9858581cfca4a992e5
Marcus Phillips <github.com@marcusphillips.com>
2013-03-07 00:32:53 -0800

    extra credit moved up, hash table fleshed out

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,22 +1,53 @@
 // Note: don't use an object to store the inserted elements.
 var makeHashTable = function(){
-  var storage = [];
-  var hashTable = {
-    //fixme
+  var limit = 8;
+  var limitedStorage = makeLimitedStorage(limit);
+
+  var hashTable = {};
+  // fill out these methods
+
+  hashTable.insert = function(){
   };
+
+  hashTable.retrieve = function(){
+  };
+
+  hashTable.remove = function(){
+  };
+
   return hashTable;
 };
 
 
-// This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
+/*
+ * Do not edit the code below this line, except for bug fixes
+ */
 
+// This is a "hashing function". You don't need to worry about it, just use it to turn any key into an integer that is well-distributed across the range 0-max
 var getIndexBelowMaxForKey = function(str, max){
   var hash = 0;
-  if (str.length == 0) return hash;
-  for (i = 0; i < str.length; i++) {
-    hash = (hash<<5) - hash;
-    hash = hash + str.charCodeAt(i);
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
     hash = hash & hash; // Convert to 32bit integer
   }
   return hash % max;
+};
+
+
+// This data structure provides you limited storage array
+// It will ensure that you don't accidentally try to use up too much space
+var makeLimitedStorage = function(limit){
+  var storage = [];
+
+  var getterSetter = function(index, value){
+    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
+    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+    if(arguments.length === 1){
+      return storage[index];
+    }else{
+      storage[index] = value;
+    }
+  };
+
+  return getterSetter;
 };
