d70883687d09931b2e5ab46fd5ea29d6386548f7
Marcus Phillips <github.com@marcusphillips.com>
2013-03-13 13:09:20 -0700

    hash bug fix

--- src/hashTableHelpers.js
+++ src/hashTableHelpers.js
@@ -40,6 +40,7 @@ var getIndexBelowMaxForKey = function(str, max){
   for (var i = 0; i < str.length; i++) {
     hash = (hash<<5) + hash + str.charCodeAt(i);
     hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
   }
   return hash % max;
 };
