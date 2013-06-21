885b6d2a66d75ea44b85a2dbe28d8d7a092034f6
Hack Reactor Students <github@hackreactor.com>
2013-06-21 13:14:10 -0700

    Working on tree, addChild close to complete

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -11,5 +11,24 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
-  // Add more tests here to test the functionality of tree.
+  it("should create an array with one object in the parent element when addChild is invoked the first time", function() {
+    tree.addChild();
+    expect(tree.children).toEqual(jasmine.any(Array));
+    expect(typeof tree.children[0]).toEqual('object');
+  });
+
+  it("should increase length of children array by one for each added child", function() {
+    tree.addChild();
+    expect(tree.children.length).toEqual(1);
+    tree.addChild();
+    expect(tree.children.length).toEqual(2);
+  });
+
+  it("should assign a value to the child when invoking addChild with the value as its argument", function() {
+    tree.addChild('element1');
+    expect(tree.children[0].value).toEqual('element1');
+    tree.addChild('element2');
+    expect(tree.children[1].value).toEqual('element2');
+  });
+
 });
--- src/tree.js
+++ src/tree.js
@@ -3,12 +3,17 @@ var makeTree = function(){
   newTree.value = undefined;
   newTree.children = undefined;
 
+  _.extend(newTree, treeMethods);
+
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  this.children === undefined && (this.children = []);
+  this.children.push(makeTree());
+  this.children[this.children.length - 1].value = value;
 };
 
 treeMethods.contains = function(){
