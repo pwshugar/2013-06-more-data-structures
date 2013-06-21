81bcd60579cb63081b10969bb5b76ec40995bf43
Hack Reactor Students <github@hackreactor.com>
2013-06-20 20:59:11 -0700

    finished makeLinkedList

--- spec/linkedListSpec.js
+++ spec/linkedListSpec.js
@@ -16,5 +16,54 @@ describe("linkedList", function() {
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
 
+  it("should update tail when calling 'addToTail'", function() {
+    linkedList.addToTail('element');
+    expect(linkedList.tail.value).toEqual('element');
+  });
+
+  it("should update head when calling 'addToTail' the first time", function() {
+    linkedList.addToTail('element');
+    expect(linkedList.head.value).toEqual('element');
+  });
+
+  it("should update tail when adding a second node but not change head", function() {
+    linkedList.addToTail('element1');
+    linkedList.addToTail('element2');
+    expect(linkedList.head.value).toEqual('element1');
+    expect(linkedList.tail.value).toEqual('element2');
+  });
+
+  it("the first node should have a reference to the next one", function() {
+    linkedList.addToTail('element1');
+    linkedList.addToTail('element2');
+    expect(linkedList.head.next).toEqual(linkedList.tail);
+  });
+
+  it("should be able to remove a node from the list, return its value, and update head", function() {
+    linkedList.addToTail('element1');
+    linkedList.addToTail('element2');
+    linkedList.addToTail('element3');
+    expect(linkedList.removeHead()).toEqual('element1');
+    expect(linkedList.head.value).toEqual('element2');
+  });
+
+  it("contains should return 'false' for an empty list", function() {
+    expect(linkedList.contains()).toEqual(false);
+  });
+
+  it("contains should return 'false' if the passed value is not in the list", function() {
+    linkedList.addToTail('element1');
+    expect(linkedList.contains('element2')).toEqual(false);
+    expect(linkedList.contains('element3')).toEqual(false);
+  });
+
+  it("contains should return 'true' if the passed value is in the list", function() {
+    linkedList.addToTail('element1');
+    linkedList.addToTail('element2');
+    linkedList.addToTail('element3');
+    expect(linkedList.contains('element1')).toEqual(true);
+    expect(linkedList.contains('element2')).toEqual(true);
+  });
+
   // add more tests here to test the functionality of linkedList
 });
--- src/linkedList.js
+++ src/linkedList.js
@@ -4,15 +4,36 @@ var makeLinkedList = function(){
   newLinkedList.head = null;
   newLinkedList.tail = null;
 
-  newLinkedList.addToTail = function(){
+  newLinkedList.addToTail = function(value){
+    var node = makeNode(value);
+    if (!newLinkedList.head) {
+      newLinkedList.tail = node;
+      newLinkedList.head = node;
+    } else {
+      newLinkedList.tail.next = node;
+      newLinkedList.tail = node;
+    }
   };
 
   newLinkedList.removeHead = function(){
+    var temp = newLinkedList.head.value;
+    newLinkedList.head = newLinkedList.head.next;
+    return temp;
   };
 
-  newLinkedList.contains = function(){
+  newLinkedList.contains = function(value){
+    if (newLinkedList.head === null) {
+      return false;
+    }
+    var node = newLinkedList.head;
+    while(node.value !== value) {
+      if (node.next === null) {
+        return false;
+      }
+      node = node.next;
+    }
+    return true;
   };
-
   return newLinkedList;
 };
 
@@ -20,9 +41,7 @@ var makeNode = function(value){
   var newNode = {};
   newNode.value = value;
   newNode.next = null;
-
-  newNode.removeNextNode = function(){
-  };
-
+  // newNode.removeNextNode = function(){
+  // };
   return newNode;
 };
