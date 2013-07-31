1b1d24f65880d3ac8cde747cb21803d1eed1835e
Peter Shugar <pwshugar@gmail.com>
2013-07-31 14:55:20 -0700

    started work on bstree

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -1,14 +1,20 @@
 describe("binarySearchTree", function() {
-  var binarySearchTree;
+  var bstree;
 
   beforeEach(function() {
-    binarySearchTree = makeBinarySearchTree();
+    bstree = new BSTree();
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
-    expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
-    expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
+    expect(bstree.insert).toEqual(jasmine.any(Function));
+    expect(bstree.contains).toEqual(jasmine.any(Function));
+    expect(bstree.depthFirstLog).toEqual(jasmine.any(Function));
+  });
+
+  it("should create an array with one object in the parent element when insert is invoked the first time", function() {
+    bstree.insert();
+    expect(bstree.children).toEqual(jasmine.any(Array));
+    expect(typeof tree.children[0]).toEqual('object');
   });
   // add more tests here to test the functionality of binarySearchTree
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,17 @@
-var makeBinarySearchTree = function(){
+var BSTree = function(){
+  this.value = null;
+  this.parent = null;
+  this.right = null;
+  this.left = null;
 };
+
+BSTree.prototype.insert = function(value){
+  
+};
+
+BSTree.prototype.contains = function(){
+};
+
+BSTree.prototype.depthFirstLog = function(){
+};
+
