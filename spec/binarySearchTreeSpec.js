a6e2de2ea9d3c2c4402538b7d28208a627c45201
Peter Shugar <pwshugar@gmail.com>
2013-09-06 17:44:46 -0700

    finished balanced binary tree

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -1,20 +1,43 @@
 describe("binarySearchTree", function() {
-  var bstree;
+  var bst;
 
   beforeEach(function() {
-    bstree = new BSTree();
+    bst = new BST(5);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(bstree.insert).toEqual(jasmine.any(Function));
-    expect(bstree.contains).toEqual(jasmine.any(Function));
-    expect(bstree.depthFirstLog).toEqual(jasmine.any(Function));
+    expect(bst.insert).toEqual(jasmine.any(Function));
+    expect(bst.contains).toEqual(jasmine.any(Function));
+    expect(bst.depthFirstLog).toEqual(jasmine.any(Function));
   });
 
   it("should create an array with one object in the parent element when insert is invoked the first time", function() {
-    bstree.insert();
-    expect(bstree.children).toEqual(jasmine.any(Array));
-    expect(typeof tree.children[0]).toEqual('object');
+    bst.insert(4);
+    expect(typeof bst.left).toEqual('object');
+    bst.insert(6);
+    expect(typeof bst.right).toEqual('object');
   });
-  // add more tests here to test the functionality of binarySearchTree
+
+  it("should insert less values to the left, greater values to the right", function() {
+    bst.insert(4);
+    expect(typeof bst.left).toEqual('object');
+    bst.insert(7);
+    expect(typeof bst.right).toEqual('object');
+    bst.insert(3);
+    expect(typeof bst.left.left).toEqual('object');
+    bst.insert(9);
+    expect(typeof bst.right.right).toEqual('object');
+    bst.insert(6);
+    expect(typeof bst.right.left).toEqual('object');
+  });
+
+  it("should return true if tree contains value, false if it doesn't", function() {
+    bst.insert(4);
+    bst.insert(7);
+    bst.insert(3);
+    expect(bst.contains(4)).toEqual(true);
+    expect(bst.contains(10)).toEqual(false);
+  });
+
+
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,17 +1,67 @@
-var BSTree = function(){
-  this.value = null;
-  this.parent = null;
+var BST = function(value){
+  this.value = value;
   this.right = null;
   this.left = null;
 };
 
-BSTree.prototype.insert = function(value){
+BST.prototype.insert = function(value){
+  if (value === undefined || this.contains(value)){ return; }
+  if (this.value > value){
+  	!this.left ? this.left = new BST(value) : this.left.insert(value);
+  } else {
+  	!this.right ? this.right = new BST(value) : this.right.insert(value);
+  }
+};
+
+BST.prototype.contains = function(value){
+  if (this.value === value){ return true; }
+  else {
+  	if (this.left && this.left.contains(value)){ return true; }
+  	if (this.right && this.right.contains(value)){ return true; }
+  }
+  return false;
+};
+
+BST.prototype.depthFirstLog = function(){
   
 };
 
-BSTree.prototype.contains = function(){
+BST.prototype.balance = function(){
+  var arr = [];
+
+  var depthSearch = function(node){
+  	arr.push(node.value);
+    if (node.left){ depthSearch(node.left); }
+    if (node.right){ depthSearch(node.right); }
+  };
+
+  depthSearch(this);
+  arr.sort(function(a,b){ return a - b });
+  this.value = arr[Math.ceil((arr.length / 2) - 1)];
+  this.left = null;
+  this.right = null;
+
+  var rebuild = function(arr, node){
+  	var mid = Math.ceil(arr.length / 2) - 1;
+  	var leftArr = arr.slice(0, mid);
+  	var rightArr = arr.slice(mid + 1, arr.length);
+  	node.insert(leftArr[Math.ceil((leftArr.length / 2) - 1)]);
+  	node.insert(rightArr[Math.ceil((rightArr.length / 2) - 1)]);
+	if (leftArr.length === 2){
+	  node.left.right = new BST(leftArr[1]);
+	} else if (leftArr.length > 2){
+	  rebuild(leftArr, node.left);
+	}
+	if (rightArr.length === 2){
+	  node.right.right = new BST(rightArr[1]);
+	} else if (rightArr.length > 2){
+	  rebuild(rightArr, node.right);
+	}
+
+  };
+
+  rebuild(arr, this);
+
 };
 
-BSTree.prototype.depthFirstLog = function(){
-};
 
